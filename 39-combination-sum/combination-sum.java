fa46bd5f20b0ebb90be17801b629f0b7c342c884
Bhavin Devani <bhavindewani@gmail.com>
2024-09-23 11:19:29 -0700

    Time: 2 ms (83.40%) | Memory: 44.2 MB (92.43%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        recursion(candidates,target,0,new ArrayList<>(),ans);
+        return ans;
+    }
+
+     private void recursion(int[] candidates, int target, int index, List<Integer> temp, List<List<Integer>> ans) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        if (target < 0 || index == candidates.length) {
+            return;
+        }
+
+        // take
+        temp.add(candidates[index]);
+        recursion(candidates, target - candidates[index], index, temp, ans); 
+        temp.remove(temp.size() - 1);
+
+        // not_take
+        recursion(candidates, target, index + 1, temp, ans);
+    }
+}
