234c8af49613d9927b4e0e23c6593582311f7221
Bhavin Devani <bhavindewani@gmail.com>
2024-09-27 11:20:27 -0700

    Time: 19 ms (8.36%) | Memory: 58.4 MB (5.01%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -3,8 +3,24 @@ class Solution {
         int n = s.length();
         int m = t.length();
         int[][] dp = new int[n][m];
-        for(int[] temp : dp) Arrays.fill(temp, -1);
-        return recursion(n-1, m-1, s, t,dp);
+        return tabulation(n,m,s,t);
+        // for(int[] temp : dp) Arrays.fill(temp, -1);
+        // return recursion(n-1, m-1, s, t,dp);
+    }
+
+    private int tabulation(int n, int m, String s, String t){
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(i-1)==t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+        }
+        }
+        return dp[n][m];
     }
 
     private int recursion(int i, int j, String s, String t,int[][] dp){
