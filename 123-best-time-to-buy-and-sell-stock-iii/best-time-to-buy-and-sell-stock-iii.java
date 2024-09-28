dc8a7d5656f7d94772d86dd852de793cb2a16419
Bhavin Devani <bhavindewani@gmail.com>
2024-09-28 13:33:08 -0700

    Time: 74 ms (38.83%) | Memory: 64 MB (37.50%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,11 +1,30 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n =prices.length;
+        return tabulation(n, prices);
+        // int[][][] dp = new int[n+1][2][3];
+        // for(int[][] curr : dp){
+        //     for(int[] temp : curr) Arrays.fill(temp, -1);
+        // }
+        // return recursion(0, 1, 0, prices,dp);
+    }
+
+    private int tabulation(int n, int[] prices){
         int[][][] dp = new int[n+1][2][3];
-        for(int[][] curr : dp){
-            for(int[] temp : curr) Arrays.fill(temp, -1);
+        for(int index=n-1;index>=0;index--){
+            for(int buy =0;buy<=1;buy++){
+                for(int transaction=1;transaction>=0;transaction--){
+                    int profit = 0;
+                    if(buy==1){
+                        profit = Math.max(-prices[index] + dp[index+1][0][transaction], dp[index+1][buy][transaction]);
+                    }else{
+                        profit = Math.max(prices[index] + dp[index+1][1][transaction+1], dp[index+1][buy][transaction]);
+                    }
+                    dp[index][buy][transaction] = profit;
+                }
+            }
         }
-        return recursion(0, 1, 0, prices,dp);
+        return dp[0][1][0];
     }
 
     private int recursion(int index, int buy, int transaction, int[] prices,int[][][] dp){
