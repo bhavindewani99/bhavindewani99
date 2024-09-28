08a4f50932ce80a928e9a7dbfbc4f11e67d22f25
Bhavin Devani <bhavindewani@gmail.com>
2024-09-28 13:16:02 -0700

    Time: 1 ms (90.60%) | Memory: 46.2 MB (9.75%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,9 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        return tabulation(n, prices);
+        // int[][] dp = new int[n+1][2];
+        // for(int[] temp : dp) Arrays.fill(temp, -1);
+        // return recursion(0, 1, prices,dp);
+    }
+
+    private int tabulation(int n, int[] prices){
         int[][] dp = new int[n+1][2];
-        for(int[] temp : dp) Arrays.fill(temp, -1);
-        return recursion(0, 1, prices,dp);
+        for(int buy=0;buy<=1;buy++){
+            dp[n][buy]=0;
+        }
+        for(int index = n-1;index>=0;index--){
+            for(int buy = 0;buy<=1;buy++){
+                int profit = 0;
+                if(buy ==1){
+                    profit = Math.max(-prices[index] + dp[index+1][0], dp[index+1][buy]);
+                }else{
+                    profit = Math.max(prices[index] + dp[index+1][1], dp[index+1][buy]);
+                }
+                dp[index][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 
     private int recursion(int index, int buy , int[] prices,int[][] dp){
