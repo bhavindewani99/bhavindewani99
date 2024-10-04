a119e40cea629f8b6fdeff30703bcc369ed776ff
Bhavin Devani <bhavindewani@gmail.com>
2024-10-03 19:22:05 -0700

    Time: 11 ms (90.91%) | Memory: 42.2 MB (75.05%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -8,9 +8,28 @@ class Solution {
         }
         newCuts[c+1] = n;
         Arrays.sort(newCuts);
-        int[][] dp = new int[c+1][c+1];
-        for(int[] temp : dp) Arrays.fill(temp, -1);
-        return recursion(1, c, newCuts, n,dp);
+        return tabulation(c, n, newCuts);
+        // int[][] dp = new int[c+1][c+1];
+        // for(int[] temp : dp) Arrays.fill(temp, -1);
+        // return recursion(1, c, newCuts, n,dp);
+    }
+
+    private int tabulation(int c, int n, int[] newCuts){
+        int[][] dp = new int[c+2][c+2];
+        
+        for(int i=c;i>=1;i--){
+            for(int j=1;j<=c;j++){
+                if(i>j) continue;
+                int cost = Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++){
+                    int curr_cost = newCuts[j+1] - newCuts[i-1] + dp[i][k-1] + dp[k+1][j];
+                    cost = Math.min(cost,curr_cost);
+                }
+                dp[i][j] = cost;
+            }
+        }
+
+        return dp[1][c];
     }
 
     private int recursion(int i, int j, int[] newCuts, int n, int[][] dp){
