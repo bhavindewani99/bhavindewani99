03358c08c956594b682c33f8b9d344f36c666e25
Bhavin Devani <bhavindewani@gmail.com>
2024-10-08 11:12:14 -0700

    Time: 301 ms (11.87%) | Memory: 45.1 MB (13.86%) - LeetSync

---
+++ 686-repeated-string-match/repeated-string-match.java
@@ -0,0 +1,59 @@
+class Solution {
+    private final int PRIME = 101;
+    public int repeatedStringMatch(String a, String b) {
+        
+        if(b.equals("")) return 0;
+        int m = a.length();
+        int n = b.length();
+        int count = 1;
+        String source = a;
+
+        while(source.length()<b.length()){
+            source+=a;
+            count++;
+        }
+
+        double sourceHash = calculateHash(source.substring(0,n));
+        double bHash = calculateHash(b);
+
+        if(checkStrng(source,b,sourceHash,bHash)) return count;
+
+        source+=a;
+        count++;
+
+        sourceHash = calculateHash(source.substring(0,n));
+        if(checkStrng(source,b,sourceHash,bHash)) return count;
+
+        return -1;
+
+    }
+
+    private double updateHash(double oldHash, char newChar, char oldChar, int patternLength){
+        double newHash = (oldHash - oldChar) / PRIME;
+        newHash += (newChar*Math.pow(PRIME, patternLength-1));
+        return newHash;
+    }
+
+    private boolean checkStrng(String source, String b, double sourceHash, double bHash){
+
+        for(int i=0;i<=source.length()-b.length();i++){
+            if(sourceHash==bHash){
+                if(source.substring(i,i+b.length()).equals(b)){
+                    return true;
+                }
+            }
+            if(i+b.length()<source.length()){
+                sourceHash = updateHash(sourceHash, source.charAt(i+b.length()), source.charAt(i), b.length());
+            }
+        }
+        return false;
+    }
+
+    private double calculateHash(String s){
+        double hash = 0;
+        for(int i =0;i<s.length();i++){
+            hash = hash + s.charAt(i) * Math.pow(PRIME, i);
+        }
+        return hash;
+    }
+}
