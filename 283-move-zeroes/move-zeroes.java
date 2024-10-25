52e306f72d90699b887324775488945da173d362
Bhavin Devani <bhavindewani@gmail.com>
2024-10-25 00:06:40 -0700

    Time: 2 ms (86.24%) | Memory: 46.2 MB (11.65%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -4,11 +4,16 @@ class Solution {
         int n = nums.length;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[index++]=nums[i];
+                if (i != index) {
+                    nums[index] = nums[i];
+                    nums[i] = 0;
+                }
+                index++;
             }
+            
         }
-        for(;index<n;index++){
-            nums[index]=0;
-        }
+        // for(;index<n;index++){
+        //     nums[index]=0;
+        // }
     }
 }
