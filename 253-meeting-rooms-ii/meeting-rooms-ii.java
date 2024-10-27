47a33c4d8ce7a5c427f97246e6c0fd75e074943a
Bhavin Devani <bhavindewani@gmail.com>
2024-10-26 22:58:00 -0700

    Time: 43 ms (5.17%) | Memory: 46.3 MB (11.67%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.java
+++ 253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -1,30 +1,21 @@
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1]-b[1]);
-        Arrays.sort(intervals, new Comparator<int[]>() {
+        Arrays.sort(intervals, new Comparator<int[]>(){
             @Override
-            public int compare(int[] a, int[] b) {
-                if(a[0]==b[0]){
-                    return a[1]-b[1];
-                }
-                return a[0] - b[0]; // Compare the second element of each interval
+            public int compare(int[] a, int[] b){
+                return a[0] - b[0];
             }
         });
-        int res= 1;
-        pq.offer(intervals[0]);
-        for(int i=1;i<intervals.length;i++){
-            if(pq.peek()[1]>intervals[i][0]){
-                pq.offer(intervals[i]);
-            }else{
-                while(!pq.isEmpty() && pq.peek()[1]<=intervals[i][0]){
-                    pq.poll();
-                }
-                pq.offer(intervals[i]);
-            }
-            res = Math.max(res,pq.size());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int res = 0;
+        for(int i=0;i<intervals.length;i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            System.out.print(start+" "+end+" ");
+            while(!pq.isEmpty() && pq.peek()<=start) pq.poll();
+            pq.offer(end);
+            res = Math.max(res, pq.size()); 
         }
         return res;
-
-
     }
 }
