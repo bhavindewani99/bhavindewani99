73b68d6dc659b9a3364bda040d1c3915c59a2e67
Bhavin Devani <bhavindewani@gmail.com>
2024-10-28 12:15:04 -0700

    Time: 8 ms (25.02%) | Memory: 43.4 MB (12.19%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,21 +1,29 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return recursion(s, 0, p, 0);
+        Map<String,Boolean> map = new HashMap<>();
+        return recursion(s, 0, p, 0, map);
     }
 
 
-    private boolean recursion(String s, int i, String p, int j){
+    private boolean recursion(String s, int i, String p, int j,Map<String,Boolean> map){
         if(i==s.length() && j>=p.length()) return true;
         if(j>=p.length()) return false;
 
+        String key = i + "#" + j;
+        if(map.containsKey(key)) return map.get(key);
+
         boolean match = (i<s.length() && (s.charAt(i)==p.charAt(j) || p.charAt(j)=='.'));
         if(j+1 < p.length() && p.charAt(j+1)=='*'){
-            return recursion(s, i, p, j+2) || (match && recursion(s, i+1, p, j));
+            boolean res = recursion(s, i, p, j+2,map) || (match && recursion(s, i+1, p, j,map));
+            map.put(key, res);
+            return res;
         }
         if(match){
-            return recursion(s, i+1, p, j+1);
+            boolean res = recursion(s, i+1, p, j+1,map);
+            map.put(key, res);
+            return res;
         }
-
+        map.put(key, false);
         return false;
     }
 }
