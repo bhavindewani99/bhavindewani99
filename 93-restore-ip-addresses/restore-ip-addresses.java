156774070fbf8ea41a07ad3ed918f159fcb56023
Bhavin Devani <bhavindewani@gmail.com>
2024-10-28 15:51:10 -0700

    Time: 2 ms (79.26%) | Memory: 42.3 MB (71.05%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        recursion(s, new StringBuilder(), 0, 0, res);
+        return res;
+    }
+
+    private void recursion(String s, StringBuilder curr, int index, int used, List<String> res){
+        if(used>4) return;
+        if(index>=s.length()){
+            if(used==4) res.add(curr.toString());
+            return;
+        }
+
+        for(int i=index;i<s.length();i++){
+            if(s.charAt(index)=='0' && i!=index) break;
+            int val = Integer.valueOf(s.substring(index,i+1));
+            if(val<=255){
+                int len = curr.length();
+                curr.append(val);
+                if(used<3)
+                curr.append(".");
+                recursion(s, curr, i+1, used+1, res);
+                curr.setLength(len);
+            }else{
+                break;
+            }
+        }
+    }
+}
