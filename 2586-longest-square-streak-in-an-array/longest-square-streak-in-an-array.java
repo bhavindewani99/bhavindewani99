4d0cd91e4601b2ba175304285d5586781ae59ac4
Bhavin Devani <bhavindewani@gmail.com>
2024-10-29 23:33:22 -0700

    Time: 77 ms (10.23%) | Memory: 59.4 MB (63.26%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.java
@@ -5,31 +5,26 @@ class Solution {
         Set<Long> used = new HashSet<>();
         int res = -1;
 
-        // Populate the set with all unique elements from nums
         for (int i : nums) set.add((long) i);
 
-        // Check each number as a potential start of a square sequence
         for (int i : nums) {
             long start = (long) i;
             if (!used.contains(start)) {
                 int currLength = 1;
                 long x = start*start;
 
-                // Try to find the next square in sequence
                 while (set.contains(x)) {
                     used.add(x);
-                    x = x * x; // Move to the next square
+                    x = x * x;
                     currLength++;
-                     // Mark this number as used
                 }
                 
-                // Update max streak if current streak is longer
                 if (currLength >= 2) {
                     res = Math.max(res, currLength);
                 }
             }
-            // Mark the starting number as used
-            used.add(start);
+            
+            //used.add(start);
         }
 
         return res;
