6e526045a8f768f8c925b4a6aab41a19199ecdd9
Bhavin Devani <bhavindewani@gmail.com>
2024-11-01 23:19:17 -0700

    Time: 47 ms (65.95%) | Memory: 48.6 MB (84.42%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.java
@@ -1,22 +1,28 @@
 class Solution {
     public int smallestChair(int[][] times, int targetFriend) {
-       
-        Integer[] order = new Integer[times.length];
-        for(int i=0;i<times.length;i++) order[i] = i;
-        Arrays.sort(order, (a,b) -> Integer.compare(times[a][0], times[b][0]));
+        
+        int arrT = times[targetFriend][0];
+        int leavingTime = times[targetFriend][1];
+        Arrays.sort(times, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] a, int[] b){
+                if(a[0]==b[0]) return a[1]-b[1];
+                return a[0]-b[0];
+            }
+        });
         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.time - b.time);
         PriorityQueue<Integer> availableChairs = new PriorityQueue<>();
         for(int i=0;i<times.length;i++) availableChairs.offer(i);
 
-        for(int i : order){
+        for(int i=0;i<times.length;i++){
             int currTime = times[i][0];
             int currLeavingTime = times[i][1];
-            
             while(!pq.isEmpty() && pq.peek().time<=currTime){
                 Pair pair = pq.poll();
                 availableChairs.offer(pair.chair);
             }
-            if(i== targetFriend) return availableChairs.peek();
+            if(currTime==arrT && currLeavingTime==leavingTime) return availableChairs.peek();
+
             pq.offer(new Pair(currLeavingTime, availableChairs.poll()));
         }
         return -1;
