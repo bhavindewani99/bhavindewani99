a9862ff1797357317b0692629951a0703bb9900a
Bhavin Devani <bhavindewani@gmail.com>
2024-11-04 11:41:05 -0800

    Time: 1 ms (89.86%) | Memory: 42.1 MB (21.14%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -7,26 +7,18 @@ class Solution {
     }
 
     private boolean recursion(String s1, String s2, String s3, int i, int j, int k,int[][] memo){
-        if(i>=s1.length() && j>=s2.length() && k>=s3.length()) return true;
-        if(k>=s3.length()) return false;
-        if(i>=s1.length() && s2.charAt(j)!=s3.charAt(k)) return false;
-        if(j>=s2.length() && s1.charAt(i)!=s3.charAt(k)) return false;
-        if(i<s1.length() && j<s2.length() && s1.charAt(i)!=s3.charAt(k) && s2.charAt(j)!=s3.charAt(k)) return false;
+        if(i>=s1.length() && j>=s2.length()) return true;
 
         if(memo[i][j]!=-1) return memo[i][j]==1 ? true : false;
+        boolean valid = false;
 
-        if(i<s1.length() && s1.charAt(i)==s3.charAt(k) && j<s2.length() && s2.charAt(j)==s3.charAt(k)){
-            boolean curr = recursion(s1, s2, s3, i+1, j, k+1,memo) || recursion(s1, s2, s3, i, j+1, k+1,memo);
-            memo[i][j] = curr ? 1 : 0;
-            return curr;
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+            valid = valid ||  recursion(s1, s2, s3, i+1, j, k+1,memo);
         }
-        else if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
-            boolean curr = recursion(s1, s2, s3, i+1, j, k+1,memo);
-            memo[i][j] = curr ? 1 : 0;
-            return curr;
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+            valid = valid || recursion(s1, s2, s3, i, j+1, k+1,memo);
         }
-        boolean curr = recursion(s1, s2, s3, i, j+1, k+1,memo);
-         memo[i][j] = curr ? 1 : 0;
-         return curr;
+        memo[i][j] = valid ? 1 : 0;
+        return valid;
     }
 }
