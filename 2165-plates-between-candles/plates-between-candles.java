ae5d85d417df0e23715f7f1bdb305323018c2295
Bhavin Devani <bhavindewani@gmail.com>
2024-11-04 10:35:22 -0800

    Time: 9 ms (74.02%) | Memory: 81.1 MB (43.22%) - LeetSync

---
+++ 2165-plates-between-candles/plates-between-candles.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] platesBetweenCandles(String s, int[][] queries) {
+        
+        int n = s.length();
+        int[] result = new int[queries.length];
+        int[] nearestLeft = new int[n];
+        int[] nearestRight= new int[n];
+        int[] candles = new int[n];
+
+        // finding total plates
+        if(s.charAt(0)=='*') candles[0] = 1;
+        for(int i=1;i<n;i++){
+            if(s.charAt(i)=='*') candles[i] = candles[i-1] +1;
+            else candles[i] = candles[i-1];
+        }
+
+        // finding nearestLeft candles
+        int prev = -1;
+        for(int i=0;i<n;i++){
+            if(s.charAt(i)=='|') prev = i;
+            nearestLeft[i] = prev;
+        }
+
+        // finding nearestRight candles;
+        prev = -1;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)=='|') prev = i;
+            nearestRight[i] = prev;
+        }
+
+        for(int i=0;i<queries.length;i++){
+            int leftCandle = nearestRight[queries[i][0]];
+            int rightCandle = nearestLeft[queries[i][1]];
+            if(leftCandle==-1 || rightCandle==-1 || leftCandle>=rightCandle) continue;
+            result[i] = candles[rightCandle] - candles[leftCandle];
+        }
+
+        return result;
+    }
+}
