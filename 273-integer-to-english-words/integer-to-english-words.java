4ca3020ffa5b6f3c064942ccb3f3e2eb8ab32811
Bhavin Devani <bhavindewani@gmail.com>
2024-11-10 11:35:16 -0800

    Time: 9 ms (48.56%) | Memory: 42.6 MB (26.90%) - LeetSync

---
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -0,0 +1,36 @@
+class Solution {
+    private final String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", 
+                                   "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", 
+                                   "Seventeen", "Eighteen", "Nineteen"};
+    private final String[] tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    private final String[] thousands = {"", "Thousand", "Million", "Billion"};
+
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+
+        int i = 0;
+        StringBuilder result = new StringBuilder();
+
+        while (num > 0) {
+            if (num % 1000 != 0) {
+                result.insert(0, helper(num % 1000) + (thousands[i].isEmpty() ? "" : " " + thousands[i]) + " ");
+            }
+            num /= 1000;
+            i++;
+        }
+
+        return result.toString().trim();
+    }
+
+    private String helper(int num) {
+        if (num < 20) {
+            return ones[num];
+        } else if (num < 100) {
+            return tens[num / 10] + (num % 10 != 0 ? " " + ones[num % 10] : "");
+        } else {
+            return ones[num / 100] + " Hundred" + (num % 100 != 0 ? " " + helper(num % 100) : "");
+        }
+    }
+}
