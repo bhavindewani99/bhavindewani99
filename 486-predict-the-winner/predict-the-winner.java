8997b99070ab421bbfb654ef636a5f21b91806b5
Bhavin Devani <bhavindewani@gmail.com>
2024-11-19 16:47:50 -0800

    Time: 0 ms (100.00%) | Memory: 40.2 MB (99.60%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,19 +1,36 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        return recursion(0, nums.length - 1, nums, 1) >= 0;
+        int[][] memo = new int[nums.length][nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                memo[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        return recursion(0, nums.length - 1, nums, 1, memo) >= 0;
     }
 
-    private int recursion(int i, int j, int[] nums, int turn) {
-        if (i > j) return 0; 
+    private int recursion(int i, int j, int[] nums, int turn, int[][] memo) {
+        if (i > j) return 0;
+
+        if (memo[i][j] != Integer.MIN_VALUE) {
+            return memo[i][j];
+        }
 
         if (turn == 1) {
-            int takeLeft = nums[i] + recursion(i + 1, j, nums, -1);
-            int takeRight = nums[j] + recursion(i, j - 1, nums, -1);
-            return Math.max(takeLeft, takeRight);
+            // Player 1's turn: maximize score difference
+            int takeLeft = nums[i] + recursion(i + 1, j, nums, -1, memo);
+            int takeRight = nums[j] + recursion(i, j - 1, nums, -1, memo);
+            memo[i][j] = Math.max(takeLeft, takeRight);
         } else {
-            int takeLeft = -nums[i] + recursion(i + 1, j, nums, 1);
-            int takeRight = -nums[j] + recursion(i, j - 1, nums, 1);
-            return Math.min(takeLeft, takeRight);
+            // Player 2's turn: minimize score difference
+            int takeLeft = -nums[i] + recursion(i + 1, j, nums, 1, memo);
+            int takeRight = -nums[j] + recursion(i, j - 1, nums, 1, memo);
+            memo[i][j] = Math.min(takeLeft, takeRight);
         }
+
+        return memo[i][j];
     }
 }
