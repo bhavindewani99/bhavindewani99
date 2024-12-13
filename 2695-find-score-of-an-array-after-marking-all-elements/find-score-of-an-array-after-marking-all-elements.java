6febd2ea2edd79f0457e6053bc2a06782f8e6d52
Bhavin Devani <bhavindewani@gmail.com>
2024-12-12 19:06:19 -0800

    Time: 242 ms (13.23%) | Memory: 61.3 MB (25.46%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,33 @@
+class Solution {
+    public long findScore(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        PriorityQueue<Pair> pairs = new PriorityQueue<>((a,b) -> a.element==b.element ? a.index - b.index : a.element - b.element);
+        Set<Integer> set = new HashSet<>();
+        long score = 0;
+        
+        for(int i=0;i<n;i++){
+            pairs.offer(new Pair(i, nums[i]));
+        }
+
+        while(!pairs.isEmpty()){
+            Pair pair = pairs.poll();
+            if(set.contains(pair.index)) continue;
+            score += pair.element;
+            set.add(pair.index);
+            set.add(pair.index-1);
+            set.add(pair.index+1);
+        }
+
+        return score;
+
+    }
+
+    class Pair{
+        int index, element;
+        Pair(int index, int element){
+            this.index = index;
+            this.element = element;
+        }
+    }
+}
