7892406a5a71b87ebee758bdeb112a55ba6c313c
Bhavin Devani <bhavindewani@gmail.com>
2024-12-28 18:14:21 +0530

    Time: 4 ms (33.24%) | Memory: 42.6 MB (35.92%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -1,6 +1,5 @@
 class Solution {
     public String removeDuplicateLetters(String s) {
-        // Map to store the last occurrence of each character
         Map<Character, Integer> lastPos = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             lastPos.put(s.charAt(i), i);
