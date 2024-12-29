5da9b358399b70905a8c53025caac5f989036ed8
Bhavin Devani <bhavindewani@gmail.com>
2024-12-29 19:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (23.44%) - LeetSync

---
+++ 3270-minimum-moves-to-capture-the-queen/minimum-moves-to-capture-the-queen.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minMovesToCaptureTheQueen(int a, int b, int c, int d, int e, int f) {
+        if(a==e || b==f){
+            if(a==c && a==e && (((d-b) * (d-f))<0)) return 2;
+            if(b==d && b==f && (((c-a) * (c-e))<0)) return 2;
+            return 1;
+        }
+        if(Math.abs(c-e)==Math.abs(d-f)){
+            if(Math.abs(c-a)==Math.abs(d-b) && ((b-f) * (b-d))<0 ) return 2;
+            return 1;
+        }
+
+        return 2;
+    }
+}
