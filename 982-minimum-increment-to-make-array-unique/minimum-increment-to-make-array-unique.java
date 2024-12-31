6180809ba5197079411549aa6b33fbedae6ef9c5
Bhavin Devani <bhavindewani@gmail.com>
2024-12-31 12:55:32 +0530

    Time: 4 ms (99.80%) | Memory: 61.7 MB (5.36%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,5 +1,30 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
+        return linearSolution(nums);
+    }
+
+    private int linearSolution(int[] nums){
+        int n = nums.length;
+        int maxi = Integer.MIN_VALUE;
+        int result = 0;
+
+        for(int i:nums) maxi = Math.max(maxi, i);
+        int[] freq = new int[maxi+n];
+
+        for(int i:nums) freq[i]++;
+
+        for(int i=0;i<maxi+n;i++){
+            if(freq[i]>1){
+                int extra = freq[i]-1;
+                result += extra;
+                freq[i+1] += extra;
+            }
+        }
+
+        return result;
+    }
+
+    private int sortSolution(int[] nums){
         int n = nums.length;
         Arrays.sort(nums);
         int result = 0;
