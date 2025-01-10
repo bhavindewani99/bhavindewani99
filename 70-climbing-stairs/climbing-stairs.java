261ca11d778742ff415d465220b4c6f9e8453194
Bhavin Devani <bhavindewani@gmail.com>
2025-01-10 18:10:00 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (92.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,8 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
+        return tabulation(n);
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return recursion(n, dp);
+    }
+
+    private int tabulation(int n){
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return recursion(n, dp);
+        dp[0]=1;
+        dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+
     }
 
     private int recursion(int n, int[] dp){
