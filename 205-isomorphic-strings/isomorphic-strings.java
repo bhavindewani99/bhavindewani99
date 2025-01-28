85795582e6dec442c8c2f45d55920babb687103a
Bhavin Devani <bhavindewani@gmail.com>
2025-01-27 19:04:39 -0800

    Time: 8 ms (79.93%) | Memory: 42.6 MB (37.85%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] map = new int[256];
+        for(int i=0;i<s.length();i++){
+            if(map[s.charAt(i)]!=map[(t.charAt(i))+128]) return false;
+            map[s.charAt(i)]=i+1;
+            map[(t.charAt(i))+128]=i+1;
+        }
+        return true;
+    }
+}
