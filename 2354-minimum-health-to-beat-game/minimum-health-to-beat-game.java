05ac483b1bc398f0cf5742a084d22c8f8a3dfff6
Bhavin Devani <bhavindewani@gmail.com>
2025-02-04 17:19:29 -0800

    Time: 2 ms (96.15%) | Memory: 57.8 MB (60.58%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.java
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.java
@@ -1,5 +1,7 @@
 class Solution {
     public long minimumHealth(int[] damage, int armor) {
+        // we will try to give armor to the element which is taking maximum health
+        // then we subtract the health we provided using the armor from the total
         long sum = 0;
         int maxi = 0;
 
