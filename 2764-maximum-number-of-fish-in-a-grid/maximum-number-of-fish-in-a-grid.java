a61eb0bfec525218c477fd8b7fa1594a29eef702
Bhavin Devani <bhavindewani@gmail.com>
2025-02-13 18:34:43 -0800

    Time: 5 ms (65.36%) | Memory: 45.5 MB (13.05%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.java
@@ -9,9 +9,7 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(visited[i][j]==false && grid[i][j]>0){
-                    int[] curr = {0};
-                    
-                    result = Math.max(result, dfs(i, j, grid, curr, m, n, visited));
+                    result = Math.max(result, dfs(i, j, grid, m, n, visited));
                 }
             }
         }
@@ -19,15 +17,17 @@ class Solution {
         return result;
     }
 
-    private int dfs(int r, int c, int[][] grid, int[] curr, int m, int n, boolean[][] visited){
+    private int dfs(int r, int c, int[][] grid, int m, int n, boolean[][] visited){
         if(r<0 || c<0 || r>=m || c>=n || grid[r][c]==0 || visited[r][c]==true) return 0;
+
         int total = grid[r][c];
         visited[r][c] = true;
-        //curr[0] += grid[r][c];
-        total += dfs(r+1, c, grid, curr, m, n, visited);
-        total += dfs(r, c+1, grid, curr, m, n, visited);
-        total += dfs(r-1, c, grid, curr, m, n, visited);
-        total += dfs(r, c-1, grid, curr, m, n, visited);
+
+        total += dfs(r+1, c, grid, m, n, visited);
+        total += dfs(r, c+1, grid, m, n, visited);
+        total += dfs(r-1, c, grid, m, n, visited);
+        total += dfs(r, c-1, grid, m, n, visited);
+
         return total;
     }
 }
