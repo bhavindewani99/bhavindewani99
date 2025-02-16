700f5a052433eccb90744f6c6059c5eeccaf7ece
Bhavin Devani <bhavindewani@gmail.com>
2025-02-15 20:01:06 -0800

    Time: 532 ms (82.73%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,7 @@
+SELECT seat_id
+FROM Cinema
+WHERE free = 1 AND(
+    seat_id - 1 IN (SELECT seat_id FROM Cinema WHERE free = 1)
+    OR
+    seat_id + 1 IN (SELECT seat_id FROM Cinema WHERE free = 1)
+);
