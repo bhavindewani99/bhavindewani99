40596b4b57875332e1030b11ba2dc2c4bee53322
Bhavin Devani <bhavindewani@gmail.com>
2025-02-15 20:16:07 -0800

    Time: 1740 ms (59.71%) | Memory: 0B (100.00%) - LeetSync

--- 1172-sales-analysis-i/sales-analysis-i.sql
+++ 1172-sales-analysis-i/sales-analysis-i.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
 SELECT seller_id
-FROM Sales s
+FROM Sales
 GROUP BY seller_id
-HAVING SUM(price) = (SELECT SUM(price)
-					 FROM Sales
-                     GROUP BY seller_id
-                     ORDER BY 1 DESC
-                     LIMIT 1 )
+HAVING SUM(price) = (SELECT SUM(price) AS prices
+                FROM Sales
+                GROUP BY seller_id
+                ORDER BY prices DESC
+                LIMIT 1
+);
