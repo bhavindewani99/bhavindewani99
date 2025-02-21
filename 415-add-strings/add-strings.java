b01bc04d64889303714b57c552267867d8193e4d
Bhavin Devani <bhavindewani@gmail.com>
2025-02-21 14:38:18 -0800

    Time: 2 ms (62.73%) | Memory: 42.7 MB (61.83%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,24 +1,19 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int i = num1.length()-1;
-        int j = num2.length()-1;
-        int carry = 0;
-        StringBuilder res = new StringBuilder();
 
-        while(i>=0 || j>=0 || carry>0){
-            int sum = carry;
-            if(i>=0){
-                sum += (num1.charAt(i)-'0');
-                i--;
-            }
-            if(j>=0){
-                sum += (num2.charAt(j)-'0');
-                j--;
-            }
-            carry = sum/10;
-            sum = sum% 10;
-            res.append(sum);
-        }
-        return res.reverse().toString();
+       StringBuilder result = new StringBuilder();
+       int m = num1.length(), n = num2.length();
+       int carry =0, i=m-1,j=n-1;
+
+       while(i>=0 || j>=0 || carry>0){
+            int x = i>=0 ? num1.charAt(i)-'0' : 0;
+            int y = j>=0 ? num2.charAt(j)-'0' : 0;
+            int currSum = x + y + carry;
+            carry = currSum > 9 ? 1 : 0;
+            result.append(currSum%10);
+            i--;
+            j--;
+       } 
+       return result.reverse().toString();
     }
 }
