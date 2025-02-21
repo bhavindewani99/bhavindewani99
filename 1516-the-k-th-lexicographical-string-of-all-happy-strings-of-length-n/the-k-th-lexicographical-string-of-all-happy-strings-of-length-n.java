32619b86b9c44fc399c6c606aebcd5de8c02371f
Bhavin Devani <bhavindewani@gmail.com>
2025-02-20 20:27:44 -0800

    Time: 3 ms (66.69%) | Memory: 41.9 MB (66.46%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -1,5 +1,36 @@
+
+
 class Solution {
     public String getHappyString(int n, int k) {
+        return optimalSolutin(n, k);
+    }
+
+    private String optimalSolutin(int n, int k){
+        int total = 3 * (int) Math.pow(2, n-1);
+        int left = 1;
+        int right = total;
+        String choices = "abc";
+        StringBuilder result = new StringBuilder();
+
+        for(int i=0;i<n;i++){
+            int curr = left;
+            int partition_size = (right - left + 1) / choices.length();
+
+            for(char c : choices.toCharArray()){
+                if(curr<=k && k< curr + partition_size){
+                    result.append(c);
+                    choices = "abc".replace(String.valueOf(c), "");
+                    left = curr;
+                    right = curr + partition_size;
+                    break;
+                }
+                curr += partition_size;
+            }
+        }
+        return result.toString();
+    }
+
+    private String recuriveSolution(int n, int k){
         List<String> happyStrings = new ArrayList<>();
         char[] chars = {'a', 'b', 'c'};
         generateHappyStrings(new StringBuilder(), chars, n, happyStrings);
