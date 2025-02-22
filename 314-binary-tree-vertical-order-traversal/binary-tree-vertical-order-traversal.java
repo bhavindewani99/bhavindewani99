62fe5651c654a42efa60360ec627f8e24aaf4113
Bhavin Devani <bhavindewani@gmail.com>
2025-02-21 20:05:46 -0800

    Time: 3 ms (57.08%) | Memory: 43.3 MB (12.39%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -1,55 +1,55 @@
 
+
+import com.sun.source.tree.Tree;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
-        
-        Map<Integer, List<Integer>> columnTable = new HashMap<>();
-        Queue<Pair> queue = new LinkedList<>();
+
         List<List<Integer>> result = new ArrayList<>();
-        
-        if (root != null) {
-            queue.offer(new Pair(root, 0));
-        }else{
-            return result;
-        }
-        
-        int minColumn = 0;
-        int maxColumn = 0;
+        if(root==null) return result;
+        TreeMap<Integer, List<Integer>> treeMap = new TreeMap<>();
+        Queue<Pair> queue = new LinkedList<>();
+
+        queue.offer(new Pair(root, 0));
 
         while (!queue.isEmpty()) {
             Pair pair = queue.poll();
             TreeNode node = pair.node;
-            int column = pair.column;
+            int level = pair.level;
 
-            columnTable.putIfAbsent(column, new ArrayList<>());
-            columnTable.get(column).add(node.val);
-            
-            minColumn = Math.min(minColumn, column);
-            maxColumn = Math.max(maxColumn, column);
+            treeMap.putIfAbsent(level, new ArrayList<>());
+            treeMap.get(level).add(node.val);
 
-            if (node.left != null) {
-                queue.offer(new Pair(node.left, column - 1));
-            }
-            if (node.right != null) {
-                queue.offer(new Pair(node.right, column + 1));
-            }
+            if(node.left!=null) queue.offer(new Pair(node.left, level-1));
+            if(node.right!=null) queue.offer(new Pair(node.right, level+1));
         }
 
-        
-        for (int i = minColumn; i <= maxColumn; i++) {
-            result.add(columnTable.get(i));
-        }
+        for(List<Integer> list : treeMap.values()) result.add(list);
 
         return result;
     }
 
-    // Pair class to hold the node and its corresponding column index
-    static class Pair {
+    class Pair{
         TreeNode node;
-        int column;
-
-        Pair(TreeNode node, int column) {
+        int level;
+        Pair(TreeNode node, int level){
             this.node = node;
-            this.column = column;
+            this.level = level;
         }
     }
 }
