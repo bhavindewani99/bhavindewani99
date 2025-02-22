90c654dbd942ae8b4849919056dc1e18a6bf2740
Bhavin Devani <bhavindewani@gmail.com>
2025-02-22 10:58:53 -0800

    Time: 17 ms (49.75%) | Memory: 46.3 MB (60.51%) - LeetSync

---
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        
+        Stack<Integer> stack = new Stack<>(); // run 2 times without emptying stack so will get next greater in the 2 time for circular array
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i=2*n-1;i>=0;i--){
+            while(!stack.isEmpty() && stack.peek()<=nums[i%n]) stack.pop();
+            if(i<n)
+            result[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.add(nums[i%n]);
+        }
+
+        return result;
+    }
+}
