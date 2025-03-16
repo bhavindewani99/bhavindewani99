5b6afc47ea1f2cdede58fc8703a51c370ac29862
Bhavin Devani <bhavindewani@gmail.com>
2025-03-16 07:37:00 -0700

    Time: 1 ms (97.79%) | Memory: 45.6 MB (76.25%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,33 +1,14 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-
-        if(n==0) return true;
-
-        if(flowerbed.length==1) {
-            if(flowerbed[0]==0) return true;
-            return false;
-        }
+        
         for(int i=0;i<flowerbed.length;i++){
             if(flowerbed[i]==0){
-                if(i==0){
-                    if(flowerbed[i+1]==0) {
-                        n--;
-                        flowerbed[i]=1;
-                    }
-                }else if(i==flowerbed.length-1){
-                    if(flowerbed[i-1]==0) {
-                        n--;
-                        flowerbed[i]=1;
-                    }
-                }else{
-                    if(flowerbed[i-1]==0 && flowerbed[i+1]==0) {
-                        n--;
-                        flowerbed[i]=1;
-                    }
+                if((i==0 || flowerbed[i-1]==0) && (i==flowerbed.length-1 || flowerbed[i+1]==0)){
+                    i++;
+                    n--;
                 }
             }
-            if(n==0) return true;
         }
-        return false;
+        return n<=0;
     }
 }
