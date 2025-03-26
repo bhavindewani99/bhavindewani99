c51e41e575eb6e5e1bcb3d33e16b11388e27ecfe
Bhavin Devani <bhavindewani@gmail.com>
2025-03-25 20:50:59 -0700

    Time: 1 ms (100.00%) | Memory: 42.7 MB (34.15%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,26 +1,30 @@
 class Solution {
     public int myAtoi(String s) {
+        
+        
+        int result = 0;
+        int n = s.length(), index = 0;
+        boolean isNegative = false;
 
-        int start = 0;
-        int n = s.length();
-        boolean isNeg = false;
-        int ans = 0;
-
-        while(start<n && s.charAt(start)==' ') start++;
-        if(start<n && s.charAt(start)=='+') start++;
-        else if(start<n && s.charAt(start)=='-'){
-            isNeg=true;
-            start++;
-        } 
-        while(start<n && Character.isDigit(s.charAt(start))){
-            int val = s.charAt(start)-'0';
-            if(ans>Integer.MAX_VALUE/10 || (ans==Integer.MAX_VALUE/10 && val>Integer.MAX_VALUE%10)){
-                return isNeg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-            }
-            ans = ans*10 + val;
-            start++;
+        while (index<n && s.charAt(index)==' ') index++;
+        if(index<n && s.charAt(index)=='-'){
+            isNegative=true;
+            index++;
         }
-        return isNeg ? -ans : ans;
+        else if(index<n && s.charAt(index)=='+') index++;
 
+        while (index<n && Character.isDigit(s.charAt(index))) {
+
+            int num = (s.charAt(index)- '0');
+
+            if(result>Integer.MAX_VALUE/10 || (result==Integer.MAX_VALUE/10 && num>Integer.MAX_VALUE%10)){
+                return isNegative ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
+
+            result = result*10 + num;
+            index++;
+        }
+
+        return isNegative ? -result : result;
     }
 }
