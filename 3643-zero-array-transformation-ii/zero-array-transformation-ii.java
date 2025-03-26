bf8eae646b85221e1161edb347467cbbd0bd2cc7
Bhavin Devani <bhavindewani@gmail.com>
2025-03-25 20:31:07 -0700

    Time: 3 ms (100.00%) | Memory: 99.8 MB (91.64%) - LeetSync

--- 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -1,39 +1,31 @@
 class Solution {
+
     public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        int l = 0;
-        int r = queries.length;
-        int result = -1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            if(possible(nums,queries,n,mid)){
-                result = mid;
-                r = mid-1;
-            }else{
-                l = mid+1;
+        int n = nums.length, sum = 0, k = 0;
+        int[] differenceArray = new int[n + 1];
+
+        // Iterate through nums
+        for (int index = 0; index < n; index++) {
+            // Iterate through queries while current index of nums cannot equal zero
+            while (sum + differenceArray[index] < nums[index]) {
+                k++;
+
+                // Zero array isn't formed after all queries are processed
+                if (k > queries.length) {
+                    return -1;
+                }
+                int left = queries[k - 1][0], right = queries[k - 1][1], val =
+                    queries[k - 1][2];
+
+                // Process start and end of range
+                if (right >= index) {
+                    differenceArray[Math.max(left, index)] += val;
+                    differenceArray[right + 1] -= val;
+                }
             }
+            // Update prefix sum at current index
+            sum += differenceArray[index];
         }
-        return result;
-    }
-
-    private boolean possible(int[] nums, int[][] queries, int n, int k){
-        int[] prefix = new int[n+1];
-
-        for(int i=0;i<k;i++){
-            int l = queries[i][0];
-            int r = queries[i][1];
-            int val = queries[i][2];
-            prefix[l] += val;
-            prefix[r+1] -= val;
-        }
-
-        for(int i=1;i<n;i++){
-            prefix[i] += prefix[i-1];
-        }
-
-        for(int i=0;i<n;i++){
-            if(nums[i]>prefix[i]) return false;
-        }
-        return true;
+        return k;
     }
 }
