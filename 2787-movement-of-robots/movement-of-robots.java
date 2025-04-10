972ade9c8a7b1e9de196ed87803b74a45420e8e2
Bhavin Devani <bhavindewani@gmail.com>
2025-04-10 12:46:43 -0700

    Time: 11 ms (35.59%) | Memory: 57.9 MB (73.73%) - LeetSync

--- 2787-movement-of-robots/movement-of-robots.java
+++ 2787-movement-of-robots/movement-of-robots.java
@@ -3,8 +3,7 @@ class Solution {
         int n = nums.length;
         long[] pos = new long[n];
         long MOD = 1_000_000_007;
-
-        // Apply movement based on directions
+        
         for (int i = 0; i < n; i++) {
             pos[i] = nums[i];
             if (s.charAt(i) == 'R') {
@@ -23,9 +22,6 @@ class Solution {
             result = (result + i * pos[i] - prefixSum) % MOD;
             prefixSum += pos[i];
         }
-
-        // Ensure non-negative result due to Java's % behavior
-        //if (result < 0) result += MOD;
 
         return (int) result;
     }
