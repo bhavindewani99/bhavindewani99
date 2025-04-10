d2915ff2655efb89cdf34b66244cdc0f90a37da3
Bhavin Devani <bhavindewani@gmail.com>
2025-04-10 10:55:32 -0700

    Time: 97 ms (32.20%) | Memory: 55.7 MB (63.56%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -1,50 +1,45 @@
 class Robot {
-
-    private int width, height;
-    private int x, y;
-    private int dir; // 0 = East, 1 = North, 2 = West, 3 = South
-    private int[][] directions = {{1,0}, {0,1}, {-1,0}, {0,-1}};
-    private String[] dirNames = {"East", "North", "West", "South"};
+    private int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    private String[] direction = {"East","North","West","South"};
+    private int width;
+    private int height;
+    private int x,y,d,mod;
 
     public Robot(int width, int height) {
         this.width = width;
         this.height = height;
-        this.x = 0;
-        this.y = 0;
-        this.dir = 0; // Start facing East
+        mod = width*2 + height*2 -4;
+        d=0;
+    }
+
+    private boolean isValid(int x, int y){
+        return x>=0 && x<width && y>=0 && y<height;
     }
 
     public void step(int num) {
-        int perimeter = 2 * (width + height) - 4;
-        num = num % perimeter;
+        num = num % mod;
+        if(num == 0) num=mod;
 
-        while (num > 0) {
-            int maxSteps = 0;
-
-            if (dir == 0) maxSteps = width - 1 - x;                  // East
-            else if (dir == 1) maxSteps = height - 1 - y;            // North
-            else if (dir == 2) maxSteps = x;                         // West
-            else if (dir == 3) maxSteps = y;                         // South
-
-            int step = Math.min(num, maxSteps);
-            x += directions[dir][0] * step;
-            y += directions[dir][1] * step;
-            num -= step;
-
-            if (num > 0) { // Turn clockwise
-                dir = (dir + 1) % 4;
-            }
+        while(num-->0){
+            if(!isValid(x+dir[d][0], y+dir[d][1])) d = (d+1)%4;
+            x+=dir[d][0];
+            y+=dir[d][1];
         }
-
-        // Special case: if we land at origin after a full loop, direction should be South
-        if (x == 0 && y == 0 && num == 0) dir = 3;
     }
-
+    
     public int[] getPos() {
-        return new int[]{x, y};
+        return new int[]{x,y};
     }
-
+    
     public String getDir() {
-        return dirNames[dir];
+        return direction[d];
     }
 }
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */
