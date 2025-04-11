5f8cdd4131b2ea1aed06ac05136b7def90404ca6
Bhavin Devani <bhavindewani@gmail.com>
2025-04-11 10:12:52 -0700

    Time: 1 ms (96.22%) | Memory: 40.3 MB (87.29%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -3,10 +3,20 @@ class Solution {
         
         int sum = 0;
 
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0) sum += i;
-        }
+        for(int i=1;i * i<=num;i++){
+            if(num%i==0){
+                sum += i;
+                if(i*i!=num ){
+                    sum += num/i;
+                }
 
-        return sum == num;
+            }
+        }
+        return sum - num == num;
     }
 }
+
+// 28 -> Sqrt is 5
+// 1 
+// 2, 14
+// 4, 7 
