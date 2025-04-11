4e48d54d1ba3a1be03f8c0450cf77658c252bc05
Bhavin Devani <bhavindewani@gmail.com>
2025-04-11 09:22:17 -0700

    Time: 3 ms (53.54%) | Memory: 43.7 MB (99.94%) - LeetSync

--- 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/minimum-operations-to-make-array-values-equal-to-k.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minOperations(int[] nums, int k) {
         
-        TreeSet<Integer> treeSet = new TreeSet<>((a,b) -> a-b);
+        Set<Integer> treeSet = new HashSet<>();
         int mini = Integer.MAX_VALUE, operations = 0;
 
         for(int i:nums){
@@ -12,7 +12,7 @@ class Solution {
         if(mini < k) return -1;
 
         operations += treeSet.size();
-        if(treeSet.first()==k) operations--;
+        if(mini == k) operations--;
         return operations;
     }
 }
