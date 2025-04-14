951c2add26856f607521a6bc79cd45fbe04cab85
Bhavin Devani <bhavindewani@gmail.com>
2025-04-13 17:27:05 -0700

    Time: 907 ms (87.16%) | Memory: 0B (100.00%) - LeetSync

--- 2842-find-active-users/find-active-users.sql
+++ 2842-find-active-users/find-active-users.sql
@@ -1,9 +1,11 @@
-# Write your MySQL query statement below
 WITH row_user AS (
-    SELECT *, ROW_NUMBER() OVER() AS rn FROM Users
+    SELECT user_id, created_at,
+           ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY created_at) AS rn
+    FROM Users
 )
-
-SELECT DISTINCT(a.user_id) 
-FROM row_user a, row_user b 
-WHERE a.user_id = b.user_id AND a.rn <> b.rn AND 
-ABS(DATEDIFF(b.created_at, a.created_at)) <= 7
+SELECT DISTINCT a.user_id
+FROM row_user a
+JOIN row_user b 
+    ON a.user_id = b.user_id 
+    AND a.rn < b.rn
+WHERE ABS(DATEDIFF(b.created_at, a.created_at)) <= 7;
