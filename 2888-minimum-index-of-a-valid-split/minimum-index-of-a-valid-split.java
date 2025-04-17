3e9753123f66524dbfab00e96bdff8953a142705
Bhavin Devani <bhavindewani@gmail.com>
2025-04-17 11:09:01 -0700

    Time: 62 ms (5.03%) | Memory: 57.2 MB (65.11%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.java
@@ -2,9 +2,9 @@ class Solution {
     public int minimumIndex(List<Integer> nums) {
         
         int n = nums.size();
-        int[] leftDominant = new int[n], rightDominant = new int[n];
+        int[] leftDominant = new int[n];
         Map<Integer, Integer> map = new HashMap<>(); // value , frequency
-        int currMaxFreq = 0, currMaxElement = 0;
+        int currMaxFreq = 0, currMaxElement = 0, result = -1;
 
         for(int i=0;i<n;i++){
             map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
@@ -19,19 +19,18 @@ class Solution {
         currMaxElement = 0;
         currMaxFreq = 0;
 
-        for(int i=n-1;i>=0;i--){
+        for(int i=n-1;i>=1;i--){
             map.put(nums.get(i), map.getOrDefault(nums.get(i), 0) + 1);
             if(map.get(nums.get(i)) > currMaxFreq){
                 currMaxFreq = map.get(nums.get(i));
                 currMaxElement = nums.get(i);
             }
 
-            if(currMaxFreq * 2 > n - i) rightDominant[i] = currMaxElement;
+            if(currMaxFreq * 2 > n - i && leftDominant[i-1] == currMaxElement){
+                result = i-1;
+            }
         }
-
-        for(int i=0;i<n-1;i++){
-            if(leftDominant[i]!=0 && leftDominant[i]==rightDominant[i+1]) return i;
-        }
-        return -1;
+         
+        return result;
     }
 }
