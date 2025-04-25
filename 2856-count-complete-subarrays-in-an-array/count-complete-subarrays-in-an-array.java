e7f492a89c5b53cb54da4ef11770a644f6b67713
Bhavin Devani <bhavindewani@gmail.com>
2025-04-25 10:14:53 -0700

    Time: 8 ms (58.91%) | Memory: 45 MB (62.47%) - LeetSync

---
+++ 2856-count-complete-subarrays-in-an-array/count-complete-subarrays-in-an-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int countCompleteSubarrays(int[] nums) {
+        
+        Set<Integer> totalDistinct = new HashSet<>();
+        HashMap<Integer, Integer> currDistinct = new HashMap<>();
+        int left = 0, n = nums.length, result = 0;
+
+        for(int num : nums) totalDistinct.add(num);
+
+
+        for(int right = 0; right < n; right++){
+            currDistinct.put(nums[right], currDistinct.getOrDefault(nums[right], 0)+1);
+
+            // if(currDistinct.size() == totalDistinct.size()){
+            //     result += n - right; // because adding more will also create complete array
+            // }
+
+            while(currDistinct.size() == totalDistinct.size()){
+                result += n - right; // because adding more will also create complete array
+                currDistinct.put(nums[left], currDistinct.getOrDefault(nums[left], 0)-1);
+                if(currDistinct.get(nums[left])==0) currDistinct.remove(nums[left]);
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
