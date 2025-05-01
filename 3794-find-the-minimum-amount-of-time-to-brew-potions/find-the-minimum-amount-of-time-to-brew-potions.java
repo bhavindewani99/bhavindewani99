4f0caa9a9e1a36b4dd98923188ee3961a2abf1ab
Bhavin Devani <bhavindewani@gmail.com>
2025-05-01 09:19:32 -0700

    Time: 217 ms (35.17%) | Memory: 283.6 MB (13.20%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -1,24 +1,30 @@
 class Solution {
     public long minTime(int[] skill, int[] mana) {
-        int n = skill.length, m = mana.length;
-        long dp[][] = new long[m][n];
+        
+        int skillLen = skill.length, manaLen = mana.length;
+        long[][] dp = new long[manaLen][skillLen];
 
         dp[0][0] = mana[0]*skill[0];
-        for (int i = 1; i < n; i++) {
-            dp[0][i] = dp[0][i-1] + (mana[0]*skill[i]);
+        for(int i=1;i<skillLen;i++){
+            dp[0][i] = dp[0][i-1] + mana[0]*skill[i];
         }
-        for (int i = 1; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j == 0) {
-                    dp[i][j] = dp[i-1][j]+(mana[i]*skill[j]);
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + (mana[i]*skill[j]);
+
+
+        for(int i=1;i<manaLen;i++){
+            for(int j=0;j<skillLen;j++){
+                if(j==0){
+                    dp[i][j] = dp[i-1][j] + mana[i]*skill[j];
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])+ mana[i]*skill[j];
                 }
             }
-            for (int k = n-2; k >= 0; k--) {
-                dp[i][k] = (dp[i][k+1]-(mana[i]*skill[k+1]));
+
+            // Actual Time Taken
+            for(int j=skillLen-2;j>=0;j--){
+                dp[i][j] = dp[i][j+1] - mana[i]*skill[j+1];
             }
         }
-        return dp[m-1][n-1];
+
+        return dp[manaLen-1][skillLen-1];
     }
 }
