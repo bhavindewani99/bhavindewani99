49bf25dd7b782464a95140116dc872a165767b50
Bhavin Devani <bhavindewani@gmail.com>
2025-05-01 17:13:39 -0700

    Time: 35 ms (88.41%) | Memory: 57.8 MB (44.11%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -1,41 +1,19 @@
 class Solution {
     public int minIncrementForUnique(int[] nums) {
-        return linearSolution(nums);
-    }
-
-    private int linearSolution(int[] nums){
-        int n = nums.length;
-        int maxi = Integer.MIN_VALUE;
-        int result = 0;
-
-        for(int i:nums) maxi = Math.max(maxi, i);
-        int[] freq = new int[maxi+n];
-
-        for(int i:nums) freq[i]++;
-
-        for(int i=0;i<maxi+n;i++){
-            if(freq[i]>1){
-                int extra = freq[i]-1;
-                result += extra;
-                freq[i+1] += extra;
-            }
-        }
-
-        return result;
-    }
-
-    private int sortSolution(int[] nums){
-        int n = nums.length;
-        Arrays.sort(nums);
-        int result = 0;
         
-        for(int i=1;i<n;i++){
-            if(nums[i-1]>=nums[i]){
-                result += Math.abs(nums[i-1]+1-nums[i]);
-                nums[i] = nums[i-1] + 1;
+        Arrays.sort(nums);
+        int operations = 0, next = nums[0]+1;
+
+        for(int i=1;i<nums.length;i++){
+            int curr = nums[i];
+            if(curr <= next) {
+                operations += (next-curr);
+                next++;
+            }else{
+                next = nums[i] + 1;
             }
         }
 
-        return result;
+        return operations;
     }
 }
