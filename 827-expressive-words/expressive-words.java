f3aee4bdc89a119c521fc6418d9bd043b6463e44
Bhavin Devani <bhavindewani@gmail.com>
2025-06-10 11:38:48 -0700

    Time: 1 ms (100.00%) | Memory: 41.8 MB (70.33%) - LeetSync

---
+++ 827-expressive-words/expressive-words.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int expressiveWords(String s, String[] words) {
+        int result = 0;
+        for (String word : words) {
+            if (canExtend(word, s)) result++;
+        }
+        return result;
+    }
+
+    private boolean canExtend(String word, String s) {
+        int i = 0, j = 0;
+        int m = word.length(), n = s.length();
+
+        while (i < m && j < n) {
+            if (word.charAt(i) != s.charAt(j)) return false;
+
+            // Count group length in word
+            int iStart = i;
+            while (i < m && word.charAt(i) == word.charAt(iStart)) i++;
+            int wordGroupLen = i - iStart;
+
+            // Count group length in s
+            int jStart = j;
+            while (j < n && s.charAt(j) == s.charAt(jStart)) j++;
+            int sGroupLen = j - jStart;
+
+            // Validate stretchiness
+            if (sGroupLen < 3 && wordGroupLen != sGroupLen) return false;
+            if (sGroupLen >= 3 && wordGroupLen > sGroupLen) return false;
+        }
+
+        return i == m && j == n;
+    }
+}
