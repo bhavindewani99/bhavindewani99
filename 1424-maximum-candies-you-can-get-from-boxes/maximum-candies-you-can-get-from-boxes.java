3b080d5fdb83fab02ba9b842b363f58744efe24f
Bhavin Devani <bhavindewani@gmail.com>
2025-06-19 10:26:46 -0700

    Time: 6 ms (29.48%) | Memory: 57.8 MB (12.62%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.java
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.java
@@ -1,47 +1,40 @@
 class Solution {
     public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
-        
-        Queue<Integer> queue = new LinkedList<>(); // will process boxes
-        int result = 0;
-        boolean[] boxFound = new boolean[status.length];
-        boolean[] processed = new boolean[status.length];
-        boolean[] keyFound = new boolean[status.length];
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] hasBox = new boolean[status.length];
+        boolean[] hasKey = new boolean[status.length];
+        boolean[] opened = new boolean[status.length];
+        int totalCandies = 0;
 
-        for(int box : initialBoxes){
-            if(status[box] == 1){
-                queue.add(box);
-                processed[box] = true;
-            }else{
-                boxFound[box] = true;
+        for (int box : initialBoxes) {
+            hasBox[box] = true;
+            if (status[box] == 1) {
+                queue.offer(box);
+                opened[box] = true;
             }
         }
 
         while (!queue.isEmpty()) {
-            int nextBox = queue.poll();
+            int box = queue.poll();
+            totalCandies += candies[box];
 
-            result += candies[nextBox];
-
-            for(int canOpen : containedBoxes[nextBox]){
-                if(processed[canOpen]) continue;
-                if(status[canOpen] == 1 || keyFound[canOpen]){
-                    processed[canOpen] = true;
-                    queue.add(canOpen);
-                }else{
-                    boxFound[canOpen] = true;
+            for (int contained : containedBoxes[box]) {
+                hasBox[contained] = true;
+                if ((status[contained] == 1 || hasKey[contained]) && !opened[contained]) {
+                    queue.offer(contained);
+                    opened[contained] = true;
                 }
             }
 
-            for(int key : keys[nextBox]){
-                if(processed[key]) continue;
-                if(boxFound[key]){
-                    processed[key] = true;
-                    queue.add(key);
-                }else{
-                    keyFound[key] = true;
+            for (int key : keys[box]) {
+                hasKey[key] = true;
+                if (hasBox[key] && !opened[key]) {
+                    queue.offer(key);
+                    opened[key] = true;
                 }
             }
         }
 
-        return result;
+        return totalCandies;
     }
 }
