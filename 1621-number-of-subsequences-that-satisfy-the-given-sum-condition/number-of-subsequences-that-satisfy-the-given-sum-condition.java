401a199635d4060250898f5cb40ff9ce7df01c13
Bhavin Devani <bhavindewani@gmail.com>
2025-06-29 11:10:18 -0700

    Time: 27 ms (99.52%) | Memory: 55 MB (79.79%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,44 +1,22 @@
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        long result = 0, mod = (int) 1e9 + 7;
+        
         Arrays.sort(nums);
+        int n = nums.length, result =0, left =0, right = n-1, mod = (int) 1e9 + 7;
+        int[] power = new int[n];
+        power[0] = 1;
 
-        for (int left = 0; left < nums.length; left++) {
-            if (nums[left] > target - nums[left]) break;
-            int newTarget = target - nums[left];
-            int rightIndex = binarySearch(nums, newTarget);
-
-            if (rightIndex >= left) {
-                result = (result + modPow(2, rightIndex - left, mod)) % mod;
-            }
+        for(int i=1;i<n;i++){
+            power[i] = (power[i-1] * 2) % mod;
         }
-        return (int) result;
-    }
 
-    private int binarySearch(int[] nums, int target) {
-        int left = 0, right = nums.length - 1, index = -1;
-
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] <= target) {
-                index = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        while (left<=right) {
+            if(nums[left] + nums[right] <= target){
+                result = (result + power[right - left]) % mod;
+                left++;
+            }else{
+                right--;
             }
-        }
-        return index;
-    }
-
-    private long modPow(long base, int exp, long mod) {
-        long result = 1;
-        //base %= mod;
-        while (exp > 0) {
-            if ((exp & 1) == 1) {
-                result = (result * base) % mod;
-            }
-            base = (base * base) % mod;
-            exp >>= 1;
         }
         return result;
     }
