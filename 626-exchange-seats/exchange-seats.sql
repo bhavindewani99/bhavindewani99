f3bcc08499c4ab330e4398fece07c9a572802834
Bhavin Devani <bhavindewani@gmail.com>
2025-08-03 09:58:14 -0700

    Time: 366 ms (30.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 626-exchange-seats/exchange-seats.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT 
+  CASE 
+    WHEN id % 2 = 0 THEN id-1
+    WHEN id % 2 <> 0 AND id < (SELECT COUNT(id) FROM seat) THEN id+1
+    ELSE id
+    END AS id,
+    student FROM seat
+    ORDER BY id;
+
