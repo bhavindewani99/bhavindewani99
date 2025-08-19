65c3eb92c4356711a2f63d6ef86b1aaf4b1dc756
Bhavin Devani <bhavindewani@gmail.com>
2025-08-19 14:22:31 -0700

    Time: 754 ms (40.20%) | Memory: 0B (100.00%) - LeetSync

--- 1974-find-customers-with-positive-revenue-this-year/find-customers-with-positive-revenue-this-year.sql
+++ 1974-find-customers-with-positive-revenue-this-year/find-customers-with-positive-revenue-this-year.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 SELECT customer_id
-FROM Customers
-WHERE year = 2021 AND revenue > 0;
+FROM Customers 
+WHERE year = 2021
+GROUP BY customer_id
+HAVING SUM(revenue) > 0;
