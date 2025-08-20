9b3e7774ee4a080d24dfac1952c2c81d4fe1802c
Bhavin Devani <bhavindewani@gmail.com>
2025-08-19 18:40:22 -0700

    Time: 1193 ms (66.43%) | Memory: 0B (100.00%) - LeetSync

--- 1480-movie-rating/movie-rating.sql
+++ 1480-movie-rating/movie-rating.sql
@@ -1,24 +1,28 @@
 WITH CTE AS(
-    SELECT u.name, 
-    DENSE_RANK() OVER (ORDER BY COUNT(mr.user_id) DESC, u.name ASC) AS rnk 
-    FROM Users u 
-    JOIN MovieRating mr
+    SELECT u.name, DENSE_RANK () OVER (ORDER BY COUNT(mr.rating) DESC, u.name) AS rnk 
+    FROM users u 
+    JOIN MovieRating mr 
     ON u.user_id = mr.user_id
     GROUP BY u.name
+   
+  
+   
 ),
-CTE2 AS( 
-    SELECT m.title,
-    DENSE_RANK() OVER (ORDER BY AVG(mr.rating) DESC, m.title ASC) AS rnk2 
-    FROM Movies m 
-    JOIN MovieRating mr
+CTE2 AS(
+    SELECT m.title, DENSE_RANK() OVER (ORDER BY AVG(mr.rating) DESC, m.title ASC) AS rnk 
+    FROM movies m 
+    JOIN MovieRating mr 
     ON m.movie_id = mr.movie_id
-    WHERE mr.created_at BETWEEN '2020-02-01' AND '2020-02-29'
-    GROUP BY m.title)
+    WHERE mr.created_at >= '2020-02-01' AND mr.created_at < '2020-03-01'
+    GROUP BY m.title
 
- SELECT name AS results
- FROM CTE
- WHERE rnk = 1
- UNION ALL
- SELECT title AS results
- FROM CTE2
- WHERE rnk2 = 1;
+)
+
+SELECT name AS results
+FROM CTE 
+WHERE rnk = 1
+
+UNION ALL
+SELECT title AS results
+FROM CTE2
+WHERE rnk = 1;
