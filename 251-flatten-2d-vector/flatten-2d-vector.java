1b531d801cf30110ff27a863dc68b0b680b379f4
Bhavin Devani <bhavindewani@gmail.com>
2025-08-24 16:16:23 -0700

    Time: 9 ms (88.71%) | Memory: 48.5 MB (49.87%) - LeetSync

--- 251-flatten-2d-vector/flatten-2d-vector.java
+++ 251-flatten-2d-vector/flatten-2d-vector.java
@@ -1,27 +1,29 @@
+import java.util.NoSuchElementException;
 class Vector2D {
 
-    List<Integer> list;
-    int index;
-    public Vector2D(int[][] vec) {
-        list = new ArrayList<>();
-        for(int[] temp : vec){
-            for(int ele : temp) list.add(ele);
+    private int[][] vector;
+    private int inner = 0;
+    private int outer = 0;
+
+    public Vector2D(int[][] v) {
+        vector = v;
+    }
+
+   
+    private void advanceToNext() {
+        while (outer < vector.length && inner == vector[outer].length) {
+            inner = 0;
+            outer++;
         }
-        index = 0;
     }
-    
+
     public int next() {
-        return list.get(index++);
+        if (!hasNext()) throw new NoSuchElementException();
+        return vector[outer][inner++];
     }
-    
+
     public boolean hasNext() {
-        return index < list.size();
+        advanceToNext();
+        return outer < vector.length;
     }
 }
-
-/**
- * Your Vector2D object will be instantiated and called as such:
- * Vector2D obj = new Vector2D(vec);
- * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
- */
