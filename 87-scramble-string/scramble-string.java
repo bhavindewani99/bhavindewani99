a8c3856eca9c726c51dfade6d0b5b0fd8d412df6
Bhavin Devani <bhavindewani@gmail.com>
2025-10-03 19:42:14 -0500

    Time: 10 ms (58.27%) | Memory: 45.2 MB (47.24%) - LeetSync

---
+++ 87-scramble-string/scramble-string.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    private Map<String, Boolean> memo = new HashMap<>();
+
+    public boolean isScramble(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        if (s1.equals(s2)) return true;
+        return recursion(s1, s2);
+    }
+
+    private boolean recursion(String s1, String s2) {
+        String key = s1 + "_" + s2;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        if (s1.equals(s2)) {
+            memo.put(key, true);
+            return true;
+        }
+
+        // pruning: if characters differ, can't be scramble
+        char[] a = s1.toCharArray(), b = s2.toCharArray();
+        Arrays.sort(a); Arrays.sort(b);
+        if (!Arrays.equals(a, b)) {
+            memo.put(key, false);
+            return false;
+        }
+
+        int n = s1.length();
+        for (int i = 1; i < n; i++) {
+            // No swap
+            if (recursion(s1.substring(0, i), s2.substring(0, i)) &&
+                recursion(s1.substring(i), s2.substring(i))) {
+                memo.put(key, true);
+                return true;
+            }
+            // Swap
+            if (recursion(s1.substring(0, i), s2.substring(n - i)) &&
+                recursion(s1.substring(i), s2.substring(0, n - i))) {
+                memo.put(key, true);
+                return true;
+            }
+        }
+
+        memo.put(key, false);
+        return false;
+    }
+}
