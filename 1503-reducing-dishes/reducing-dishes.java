089ad161e68ecbe6f0485b31b2f297eb80043208
Bhavin Devani <bhavindewani@gmail.com>
2025-10-18 12:30:28 -0500

    Time: 10 ms (38.86%) | Memory: 46.3 MB (8.21%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
         
-        Long[][] dp = new Long[satisfaction.length+1][satisfaction.length+1];
+        // Long[][] dp = new Long[satisfaction.length+1][satisfaction.length+1];
         Arrays.sort(satisfaction);
-        return (int) recursion(satisfaction, 1, 0, dp);
+        // return (int) recursion(satisfaction, 1, 0, dp);
+        return  tabulation(satisfaction);
     }
 
     private long recursion(int[] satisfaction, int time, int index, Long[][] dp){
@@ -17,5 +18,21 @@ class Solution {
 
         dp[index][time] = Math.max(take, not_take);
         return Math.max(take, not_take);
+    }
+
+    private int tabulation(int[] satisfaction){
+        int n = satisfaction.length;
+        
+        long[][] dp = new long[n+1][n+2];
+
+        for(int i=n-1;i>=0;i--){
+            for(int time = n;time>=1;time--){
+                 long not_take = dp[i+1][time];
+                long take = satisfaction[i] * time + dp[i+1][time+1];
+                dp[i][time] = Math.max(take, not_take);
+            }
+        }
+
+        return (int) dp[0][1];
     }
 }
