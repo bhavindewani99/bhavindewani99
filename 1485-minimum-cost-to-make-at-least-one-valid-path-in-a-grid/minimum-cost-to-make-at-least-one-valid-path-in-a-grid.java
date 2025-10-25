04ab3cb5c592275566fb10bd5570ff823b1a5a5b
Bhavin Devani <bhavindewani@gmail.com>
2025-10-25 17:33:58 -0500

    Time: 13 ms (81.09%) | Memory: 45.2 MB (61.45%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,49 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int minCost(int[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] direction = {{0,0}, {0,1}, {0,-1}, {1,0}, {-1,0}}; // 1: right, 2: left, 3: down, 4: up
+        int[][] direction = {{0,0}, {0,1}, {0,-1}, {1,0}, {-1,0}}; // 1:right, 2:left, 3:down, 4:up
 
-        // Distance matrix
         int[][] dist = new int[m][n];
         for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
         dist[0][0] = 0;
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.moves - b.moves);
-        pq.offer(new Pair(0, 0, 0));
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offerFirst(new int[]{0, 0});
 
-        while (!pq.isEmpty()) {
-            Pair pair = pq.poll();
-            int x = pair.x, y = pair.y, moves = pair.moves;
+        while (!dq.isEmpty()) {
+            int[] cur = dq.pollFirst();
+            int x = cur[0], y = cur[1];
 
-            // If we reached destination
-            if (x == m - 1 && y == n - 1) return moves;
-
-            // Skip if we already found a cheaper path
-            if (moves > dist[x][y]) continue;
-
-            // Explore all 4 directions
             for (int d = 1; d <= 4; d++) {
                 int r = x + direction[d][0];
                 int c = y + direction[d][1];
+
                 if (r >= 0 && c >= 0 && r < m && c < n) {
-                    // Use current cell's direction for cost check
                     int cost = (grid[x][y] == d) ? 0 : 1;
-                    if (moves + cost < dist[r][c]) {
-                        dist[r][c] = moves + cost;
-                        pq.offer(new Pair(r, c, dist[r][c]));
+                    if (dist[x][y] + cost < dist[r][c]) {
+                        dist[r][c] = dist[x][y] + cost;
+                        if (cost == 0) dq.offerFirst(new int[]{r, c});
+                        else dq.offerLast(new int[]{r, c});
                     }
                 }
             }
         }
-        return -1; // Should never happen for valid grids
-    }
 
-    class Pair {
-        int x, y, moves;
-        Pair(int x, int y, int moves) {
-            this.x = x;
-            this.y = y;
-            this.moves = moves;
-        }
+        return dist[m - 1][n - 1];
     }
 }
