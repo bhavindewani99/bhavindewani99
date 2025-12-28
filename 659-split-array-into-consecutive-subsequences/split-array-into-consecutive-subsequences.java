ebfad9bfa2360c152d5aad1ed2d75650f1d99139
Bhavin Devani <bhavindewani@gmail.com>
2025-12-28 14:41:31 -0600

    Time: 28 ms (56.63%) | Memory: 48.6 MB (18.74%) - LeetSync

--- 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.java
+++ 659-split-array-into-consecutive-subsequences/split-array-into-consecutive-subsequences.java
@@ -1,43 +1,27 @@
 class Solution {
     public boolean isPossible(int[] nums) {
+        Map<Integer, Integer> countmap = new HashMap<>();
+        Map<Integer, Integer> endmap = new HashMap<>();
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>(
-            (a, b) -> a.element != b.element ? a.element - b.element : a.size - b.size
-        );
+        for(int num : nums) countmap.put(num, countmap.getOrDefault(num,0) +1);
 
-        for (int num : nums) {
+        for(int num : nums){
+            if(countmap.get(num) == 0) continue;
 
-            // Any subsequence ending before (num - 1) can never be extended now.
-            // It must already have length >= 3.
-            while (!pq.isEmpty() && pq.peek().element < num - 1) {
-                if (pq.poll().size < 3) return false;
+            // check if we can extend a subsequence
+            if(endmap.getOrDefault(num-1, 0) > 0){
+                endmap.put(num-1, endmap.getOrDefault(num-1, 0) - 1); // means there is not subsequece ending at num - 1
+                endmap.put(num, endmap.getOrDefault(num, 0) + 1); // there is a subsequenc ending at num 
+            } else if(countmap.getOrDefault(num+1,0)>0 && countmap.getOrDefault(num+2,0)> 0){ // start a new sequence
+                endmap.put(num+2, endmap.getOrDefault(num+2, 0) + 1);
+                countmap.put(num+1, countmap.getOrDefault(num+1, 0) - 1);
+                countmap.put(num+2, countmap.getOrDefault(num+2, 0) - 1);
+            } else{ // cannot proceed further
+                return false;
             }
-
-            // Extend the shortest subsequence that ends at num - 1, if it exists.
-            if (!pq.isEmpty() && pq.peek().element == num - 1) {
-                Pair p = pq.poll();
-                p.element = num;
-                p.size++;
-                pq.offer(p);
-            } else {
-                // Otherwise start a new subsequence at num
-                pq.offer(new Pair(num, 1));
-            }
-        }
-
-        // Finalize remaining subsequences
-        while (!pq.isEmpty()) {
-            if (pq.poll().size < 3) return false;
+            countmap.put(num, countmap.getOrDefault(num,0) -1);
         }
 
         return true;
-    }
-
-    class Pair {
-        int element, size;
-        Pair(int element, int size) {
-            this.element = element;
-            this.size = size;
-        }
     }
 }
