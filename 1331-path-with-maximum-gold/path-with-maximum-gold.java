b6653ef06fbc116f0f1d1838e9d67f1071c47e0c
Bhavin Devani <bhavindewani@gmail.com>
2026-01-27 12:50:26 -0600

    Time: 962 ms (5.05%) | Memory: 286.7 MB (6.38%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.java
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.java
@@ -1,38 +1,69 @@
 class Solution {
+    private final int[] DIRECTIONS = new int[] { 0, 1, 0, -1, 0 };
     public int getMaximumGold(int[][] grid) {
-        
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        int gold  = 0;
-        int[][] directions = {{-1,0} ,{0,1},{1,0},{0,-1}};
+        int rows = grid.length;
+        int cols = grid[0].length;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]!=0){
-                    gold = Math.max(gold, dfs(i,j,m,n,grid,visited,directions));
+        // Find the total amount of gold in the grid
+        int totalGold = 0;
+        for (int[] row : grid) {
+            for (int cell : row) {
+                totalGold += cell;
+            }
+        }
+
+        // Search for the path with the maximum gold starting from each cell
+        int maxGold = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (grid[row][col] != 0) {
+                    maxGold = Math.max(maxGold, bfsBacktrack(grid, rows, cols, row, col));
+                    // If we found a path with the total gold, it's the max gold
+                    if (maxGold == totalGold) {
+                        return totalGold;
+                    }
                 }
             }
         }
-        return gold;
+        return maxGold;
     }
 
-    private int dfs(int i, int j, int m , int n, int[][] grid, boolean[][] visited, int[][] directions){
-        if(i<0 || j<0 || i>=m ||j >=n || visited[i][j] || grid[i][j]==0) return 0;
+    // Helper function to perform BFS
+    private int bfsBacktrack(int[][] grid, int rows, int cols, int row, int col) {
+        Queue<Pair<int[], Set<String>>> queue = new ArrayDeque<>();
+        Set<String> visited = new HashSet<>();
+        int maxGold = 0;
+        visited.add(row + "," + col);
+        queue.offer(new Pair<>(new int[] { row, col, grid[row][col] }, visited));
 
-        int gold = grid[i][j];
-        int maxi = 0;
-        visited[i][j] = true;
+        while (!queue.isEmpty()) {
+            Pair<int[], Set<String>> current = queue.poll();
+            int currRow = current.getKey()[0];
+            int currCol = current.getKey()[1];
+            int currGold = current.getKey()[2];
+            Set<String> currVis = current.getValue();
+            maxGold = Math.max(maxGold, currGold);
 
-        for(int d=0;d<4;d++){
-            maxi = Math.max(maxi, dfs( i + directions[d][0], j+directions[d][1], m, n, grid, visited, directions));
+            // Search for gold in each of the 4 neighbor cells
+            for (int direction = 0; direction < 4; direction++) {
+                int nextRow = currRow + DIRECTIONS[direction];
+                int nextCol = currCol + DIRECTIONS[direction + 1];
+
+                // If the next cell is in the matrix, has gold,
+                // and has not been visited, add it to the queue
+                if (nextRow >= 0 && nextRow < rows && nextCol >= 0 && nextCol < cols &&
+                        grid[nextRow][nextCol] != 0 &&
+                        !currVis.contains(nextRow + "," + nextCol)) {
+                    
+                    currVis.add(nextRow + "," + nextCol);
+                    Set<String> copyVis = new HashSet<>(currVis);
+                    queue.offer(new Pair<>(new int[] { nextRow, nextCol, 
+                                           currGold + grid[nextRow][nextCol]}, copyVis));
+                    currVis.remove(nextRow + "," + nextCol);
+
+                }
+            }
         }
-
-        visited[i][j] = false;
-        return gold + maxi;
-        
+        return maxGold;
     }
-
-
-
 }
