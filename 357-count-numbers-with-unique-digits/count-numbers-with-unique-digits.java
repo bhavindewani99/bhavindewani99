a9da3c27cb74f99e47c2cc3c8d158f322f955192
Bhavin Devani <bhavindewani@gmail.com>
2026-05-07 17:37:29 -0500

    Time: 26 ms (11.42%) | Memory: 42.5 MB (11.77%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,28 +1,25 @@
 class Solution {
-    int res = 1; // Count "0" immediately
 
+    int res = 1;
     public int countNumbersWithUniqueDigits(int n) {
-        if (n == 0) return 1;
+
+        if(n==0) return res;
         
-        
-        boolean[] used = new boolean[10];
-        backtracking(0, 0, used, n);
+        backtrack(n, 0, 0, new boolean[10]);
+
         return res;
     }
 
-    private void backtracking(long curr, int digitCount, boolean[] used, int n) {
-        // Base case: Stop if we've reached the maximum allowed digits
-        if (digitCount == n) return;
+    private void backtrack(int n, int digits, int curr, boolean[] used){
+        if(digits == n) return;
 
-        for (int i = 0; i < 10; i++) {
-        
-            if (curr == 0 && i == 0) continue;
-
-            if (!used[i]) {
-                used[i] = true;
+        for(int i=0;i<10;i++){
+            if(i==0 && curr == 0) continue;
+            if(!used[i]){
                 res++;
-                backtracking(curr * 10 + i, digitCount + 1, used, n);
-                used[i] = false; // Backtrack
+                used[i] = true;
+                backtrack(n, digits+1, curr*10 + i,used);
+                used[i] = false;
             }
         }
     }
